bafabfaf0a05caaab17c34d3b74c449757eb2a1d
Manab local workspace <manabboro@hotmail.com>
2022-03-15 00:15:30 +0530

    using cmd

--- apps/students/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import AssignmentsView
-
-urlpatterns = [
-  path('assignments/', AssignmentsView.as_view(), name='students-assignments')
-]
--- fyle_classroom/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import generics
-from rest_framework.response import Response
-
-class BaseView(generics.GenericAPIView):
-    def get(self, request):
-        return Response({'status': 'ok'})
